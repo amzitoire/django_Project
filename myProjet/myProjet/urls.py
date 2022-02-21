f4218cf7c81d04754ec3ab5b051652b2bfab38ba
amzitoire <amzidou16@gmail.com>
2022-02-21 18:23:27 +0000

    user inscription update password

--- myProjet/myProjet/urls.py
+++ myProjet/myProjet/urls.py
@@ -22,5 +22,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',LoginView.as_view(),name='home'),
     path('logout/', LogoutView.as_view(),name='logout'),
+    path('inscription/',create_user,name='inscription'),
     path('biblio/', login_required(TemplateView.as_view(template_name='biblio.html'))),
+    path('update/', login_required(update_user),name='update'),
+    path('password/', login_required(changePassword_user),name='password'),
 ]
